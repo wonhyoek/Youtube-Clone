149b5d907a4664e414d0c57a1f67c5d1e224bb86
wonhyoek <epahsejtmxm@naver.com>
2020-12-13 14:30:50 +0900

    Upload Video

---
+++ server/routes/video.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+
+
+var storage = multer.diskStorage({
+    destination:  (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename:  (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.mp4') {
+            return cb(res.status(400).json({success: false, message: 'only mp4 is allowed'}), false);
+        }
+        cb(null, true)
+    }
+})
+
+var upload = multer({ storage: storage }).single("file")
+
+
+
+router.post("/uploadfiles", (req, res) => {
+
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+    })
+
+});
+
+
+
+module.exports = router;
