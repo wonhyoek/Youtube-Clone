44d0649bfd089c3830faba9a699bf5090c7ce47f
wonhyoek <epahsejtmxm@naver.com>
2020-12-17 19:56:38 +0900

    VideoUploadPage 2 Side Video

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { List, Avatar, Typography } from 'antd';
+import { List, Avatar, Typography, Row, Col } from 'antd';
 import axios from 'axios';
+import SideVideo from "./Section/SideVideo";
 
 export default (props) => {
 
@@ -25,21 +26,30 @@ export default (props) => {
 
     if(Video.writer){
         return (
-            <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-                <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
-    
-                <List.Item
-                    actions={[]}
-                >
-                    <List.Item.Meta
-                        avatar={<Avatar src={Video.writer && Video.writer.image} />}
-                        title={<a href="https://ant.design">{Video.title}</a>}
-                        description={Video.description}
-                    />
-                    <div></div>
-                </List.Item>
-    
-            </div>
+            <Row>
+            <Col lg={18} xs={24}>
+                <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+                    <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
+
+                    <List.Item
+                        actions={[]}
+                    >
+                        <List.Item.Meta
+                            avatar={<Avatar src={Video.writer && Video.writer.image} />}
+                            title={<a href="https://ant.design">{Video.title}</a>}
+                            description={Video.description}
+                        />
+                        <div></div>
+                    </List.Item>
+
+                </div>
+            </Col>
+            <Col lg={6} xs={24}>
+
+                <SideVideo />
+
+            </Col>
+        </Row>
         )
     } else {
         return <div>...Loading</div>
